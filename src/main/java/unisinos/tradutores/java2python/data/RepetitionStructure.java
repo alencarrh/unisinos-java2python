cc5139d9c513438d57760a62bcf80b7469f8876c
Christian Lima <christianmellolima@gmail.com>
2018-11-29 04:07:39 -0200

    for toString working

--- src/main/java/unisinos/tradutores/java2python/data/RepetitionStructure.java
+++ src/main/java/unisinos/tradutores/java2python/data/RepetitionStructure.java
@@ -3,6 +3,8 @@ package unisinos.tradutores.java2python.data;
 import lombok.Builder;
 import lombok.Data;
 
+import static java.util.Objects.nonNull;
+
 @Data
 @Builder
 public class RepetitionStructure extends Element {
@@ -13,5 +15,18 @@ public class RepetitionStructure extends Element {
     private final GenericBody body;
     private final Integer scope;
 
+    public String toString(){
+        final StringBuilder body = new StringBuilder();
+        if (nonNull(this.body)) {
+            this.body.getExpressoins().forEach(a -> {
+                body.append("    " + a + "\n");
+            });
+        }
 
+
+        return this.initVariables.getName() + " = " + this.initVariables.getInitValue() + "\n" +
+                "while" + this.getKeepRunningCondition() +
+                body +
+                getUpdateVariables().getExpression();
+    }
 }
